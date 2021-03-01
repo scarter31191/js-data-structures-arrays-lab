e42535d42d9c268293811d60e5fde3dfd36d59ed
scarter31191 <scarter31191@gmaile.com>
2021-02-28 21:29:59 -0500

    Done.

--- index.js
+++ index.js
@@ -1,6 +1,26 @@
 const drivers = ['Milo', 'Otis', 'Garfield'];
 
-const destructivelyAppendDriver = drivers.push()
+function destructivelyAppendDriver(name) {
+    return drivers.push('Ralph');
+}
+
+function destructivelyPrependDriver(name) {
+    return drivers.unshift('Bob');
+}
+
+function destructivelyRemoveLastDriver(name) {
+    return drivers.pop('Garfield');
+}
+
+function destructivelyRemoveFirstDriver(name) {
+    return drivers.shift('Milo')
+}
+
+function appendDriver(name) {
+    return drivers.splice(3,0,"Broom")
+}
+
+console.log()
 
 
 
